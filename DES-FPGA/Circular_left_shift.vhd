a79486f1adb0cdfbef65856c9d11eb340ffbef91
king jummai <jummai@tiuelabb6.ti.lab>
2022-11-29 17:09:08 +0000

    Circular left shift

---
+++ DES-FPGA/Circular_left_shift.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    16:48:50 11/29/2022 
+-- Design Name: 
+-- Module Name:    Circular_left_shift - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Circular_left_shift is
+end Circular_left_shift;
+
+architecture Behavioral of Circular_left_shift is
+
+begin
+
+
+end Behavioral;
+
