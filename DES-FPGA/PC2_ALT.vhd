cffb06ade3727ad8b353e0a69c54822c112ef2e6
etorkay <50207979+etorkay@users.noreply.github.com>
2022-12-01 11:53:44 +0000

    PC 1 & 2 altered

---
+++ DES-FPGA/PC2_ALT.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    09:12:01 12/01/2022 
+-- Design Name: 
+-- Module Name:    PC2_ALT - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity PC2_ALT is
+end PC2_ALT;
+
+architecture Behavioral of PC2_ALT is
+
+begin
+
+
+end Behavioral;
+
