b3c50fac80ac521e9abbbfbbf87477bf02e3a87b
AnkamahEllis <105572426+AnkamahEllis@users.noreply.github.com>
2022-12-04 23:20:25 +0000

    DES Structural Dev

--- DES-FPGA/DES.vhd
+++ DES-FPGA/DES.vhd
@@ -8,13 +8,13 @@ use IEEE.STD_LOGIC_1164.ALL;
 --entity
 entity DES is
     port(
-        DATA_INPUT: IN STD_LOGIC_VECTOR(63 downto 0);
-        KEY_INPUT: IN STD_LOGIC_VECTOR(63 downto 0);
+        DATA_INPUT: IN STD_LOGIC_VECTOR(0 to 63);
+        KEY_INPUT: IN STD_LOGIC_VECTOR(0 to 63);
 
         --control signals
         muxSel, CLK, CLR, dLoad: IN STD_LOGIC;
 
-        DATA_OUTPUT: OUT STD_LOGIC_VECTOR(63 downto 0)
+        DATA_OUTPUT: OUT STD_LOGIC_VECTOR(0 to 63)
 
     );
 end DES;
@@ -23,6 +23,12 @@ end DES;
 --architecture
 architecture Structural of DES is
     --internal signals
+	 signal ip_out : std_logic_vector(63 downto 0);
+	 signal left_bit : std_logic_vector(31 downto 0);
+	 signal right_bit : std_logic_vector(31 downto 0);
+	 signal key_out : std_logic_vector(47 downto 0);
+	 signal function_out : std_logic_vector(31 downto 0);
+	 signal xor_out : std_logic_vector(31 downto 0);
 
     --component declaration
     component IP
@@ -104,66 +110,73 @@ begin
     --port connection
     CNT0: COUNT port map(
         UP => , 
-        CLK => , 
+        CLK => CLK, 
         DONE => , 
         OUTPUT =>
     );
 
     IP1: IP port map(
-        INPUT => ,
-        OUTPUT =>
+        INPUT => DATA_INPUT ,
+        OUTPUT => ip_out
     );
 
     MUXD1: MUX GENERIC MAP(32) port map(
-        INPUT_0 => , 
-        INPUT_1 => ,
-        sel => ,
-        OUTPUT =>
+        INPUT_0 => ip_out(63 downto 32), 
+        INPUT_1 => right_bit,
+        sel => muxSel ,
+        OUTPUT => left_bit
     );
     MUXD2: MUX GENERIC MAP(32) port map(
-        INPUT_0 => , 
-        INPUT_1 => ,
-        sel => ,
-        OUTPUT =>
+        INPUT_0 => ip_out(31 downto 0), 
+        INPUT_1 => xor_out,
+        sel => muxSel,
+        OUTPUT => right_bit
     );
 
-    REGD1: regN GENERIC MAP(32) port map(
-        D => ,
-        Q => ,
-        CLK =>, 
-        CLR => , 
-        LOAD =>
-    );
-    REGD2: regN GENERIC MAP(32) port map(
-        D => ,
-        Q => ,
-        CLK =>, 
-        CLR => , 
-        LOAD =>
-    );
+    --REGD1: regN GENERIC MAP(32) port map(
+    --    D => ,
+    --    Q => ,
+    --    CLK =>, 
+    --    CLR => , 
+    --    LOAD =>
+    --);
+    --REGD2: regN GENERIC MAP(32) port map(
+    --    D => ,
+    --    Q => ,
+    --    CLK =>, 
+    --    CLR => , 
+    --    LOAD =>
+    --);
 
     FUNCT0: FUNCT port map(
-        INPUT => ,
-        KEY_IN => ,
-        FUNCT_EN => ,
-        OUTPUT =>
+        INPUT => right_bit,
+        KEY_IN => key_out,
+        FUNCT_EN => '1',
+        OUTPUT => function_out
     );
     KGEN0: KEY_GEN port map(
-        INPUT => ,
-        muxSel => , 
-        CLK => , 
+        INPUT => KEY_INPUT,
+        muxSel => muxSel, 
+        CLK => CLK, 
         rLoad =>, 
-        CLR => ,
+        CLR => CLR,
         schdl_sel => ,
-        OUTPUT => 
+        OUTPUT => key_out
     );
+	 
+	 
+	 XORF: CUST_XOR GENERIC(32) port map(
+		  INPUT1 => function_out,
+		  INPUT2 => left_bit,
+		  OUTPUT => xor_out
+	 );
 
 
 
 
     FP1: FP port map(
-        INPUT => ,
-        OUTPUT =>
+        INPUT => left_bit & xor_out,
+        OUTPUT => DATA_OUTPUT
     );
 
 
